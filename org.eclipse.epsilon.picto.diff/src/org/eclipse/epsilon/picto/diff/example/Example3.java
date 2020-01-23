061640cb2763eb5813f24d26b3c88acc9b12bc5b
wrwei <ran.wei@york.ac.uk>
2020-01-23 17:44:47 +0800

    Some minor changes

--- org.eclipse.epsilon.picto.diff/src/org/eclipse/epsilon/picto/diff/example/Example3.java
+++ org.eclipse.epsilon.picto.diff/src/org/eclipse/epsilon/picto/diff/example/Example3.java
@@ -1,22 +1,21 @@
 package org.eclipse.epsilon.picto.diff.example;
 
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.mutGraph;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import guru.nidi.graphviz.attribute.Label;
 import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.model.LinkSource;
 import guru.nidi.graphviz.model.MutableGraph;
 import guru.nidi.graphviz.model.MutableNode;
 import guru.nidi.graphviz.parse.Parser;
-
-import static guru.nidi.graphviz.model.Factory.*;
 
 public class Example3 {
 
@@ -35,26 +34,12 @@ public class Example3 {
 	    }
 	    List<MutableNode> g2ns = new ArrayList<MutableNode>();
 	    for(MutableNode n: g2.nodes()) {
-	    	n.setName(Label.of(n.name().toString()+" "));
+	    	n.setName(Label.of("_"+n.name().toString()));
 	    	g2ns.add(n);
 	    }
-	    g.add(graph("dot1").cluster().graphAttr().with("label", "dot1").with(g1ns));
+	    g.add(graph("dot1").cluster().graphAttr().with("label", "dot1").directed().with(g1ns));
 //	    		with(node("foo").link(node("bar"))));
-	    g.add(graph("dot2").cluster().graphAttr().with("label", "dot2").with(g2ns));
-//	    		with(node("foo ").with("shape", "box").link(node("bar "))));
-//	    MutableGraph g1c = g1.copy().setCluster(true);
-//	    g1c.setName("dot1");
-//	    MutableGraph g2c = g2.copy().setCluster(true);
-//	    g2c.setName("dot2");
-//	    g1c.addTo(g);
-//	    g2c.addTo(g);
-//	    MutableNode n = g.nodes().iterator().next();
-//	    g.nodes().remove(n);
-//	    System.out.println(g.nodes());
-//	    System.out.println(g.nodes().size());
-	    		
-//	    		.setCluster(true).setName("foo1");
-	    //g2.setCluster(true).setName("foo2").addTo(g);
+	    g.add(graph("dot2").cluster().graphAttr().with("label", "dot2").directed().with(g2ns));
 
 	    Graphviz.fromGraph(g).width(700).render(Format.PNG).toFile(new File("example/foo.png"));
 	    Graphviz.fromGraph(g).render(Format.DOT).toFile(new File("files/foo_.dot"));
